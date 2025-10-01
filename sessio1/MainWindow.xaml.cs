9c34ef4ac785e949b86380ee9bc4504c5459540f
AmineKhamlich <sk8.amine@gmail.com>
2025-10-01 19:59:47 +0200

    tercer commit

--- sessio1/MainWindow.xaml.cs
+++ sessio1/MainWindow.xaml.cs
@@ -23,6 +23,32 @@ namespace sessio1
         public MainWindow()
         {
             InitializeComponent();
+
+            Grid elmeuGrid = new Grid();
+            this.Content = elmeuGrid;
+            Button btn = new Button();
+            elmeuGrid.Children.Add(btn);
+            btn.Foreground = Brushes.Green;
+            WrapPanel elMeuWrapPanel = new WrapPanel();
+            TextBlock txt1 = new TextBlock();
+            TextBlock txt2 = new TextBlock();
+            TextBlock txt3 = new TextBlock();
+            txt1.FontSize = 30;
+            //txt1.Background = Brushes.Khaki;
+            txt1.Foreground = Brushes.Magenta;
+            txt1.Text = "Hola   ";
+            txt2.FontSize = 20;
+            txt2.Foreground = Brushes.Brown;
+            txt2.Text = "Mon    ";
+            txt2.FontSize = 40;
+            txt2.Foreground = Brushes.YellowGreen;
+            txt3.Text = "Hello ";
+            elMeuWrapPanel.Children.Add(txt1);
+            elMeuWrapPanel.Children.Add(txt2);
+            elMeuWrapPanel.Children.Add(txt3);
+            btn.Width = 450;
+            btn.Height = 100;
+            btn.Content = elMeuWrapPanel;
         }
     }
 }
